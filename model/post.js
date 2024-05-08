01d13dc409cfcf5bb2d3eb1a283148b44d959cde
losingone <ying30821@gmail.com>
2024-05-08 22:15:32 +0800

    feat: post model

---
+++ model/post.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const postScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, '"name" is required.'],
+  },
+  content: {
+    type: String,
+    required: [true, '"content" is required.'],
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  tags: [
+    {
+      type: String,
+      required: [true, '"tag" is required.'],
+    },
+  ],
+  type: {
+    type: String,
+    required: [true, '"type" is required.'],
+    enum: ['group', 'person'],
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  comments: {
+    type: Number,
+    default: 0,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model('Post', postScheme);
+module.exports = Post;
