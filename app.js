67537c891776ed009c973b8151b2c4ec9b3807d4
losingone <ying30821@gmail.com>
2024-05-08 22:17:55 +0800

    feat: `app.js` use post & posts routes

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const { handleError } = require('./service/handler');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const postRouter = require('./routes/post');
+
+require('./connections');
 
 const app = express();
 
@@ -18,5 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/post', postRouter);
 
+app.use((req, res, next) => {
+  next(handleError(res, 404, 'route not found'));
+});
 module.exports = app;
