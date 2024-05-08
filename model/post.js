41c185d73feba3d2e2900112fc5cd8d133128008
losingone <ying30821@gmail.com>
2024-05-08 22:53:38 +0800

    feat: hide versionKey

--- model/post.js
+++ model/post.js
@@ -1,42 +1,45 @@
 const mongoose = require('mongoose');
 
-const postScheme = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, '"name" is required.'],
-  },
-  content: {
-    type: String,
-    required: [true, '"content" is required.'],
-  },
-  image: {
-    type: String,
-    default: '',
-  },
-  tags: [
-    {
+const postScheme = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: [true, '"tag" is required.'],
+      required: [true, '"name" is required.'],
     },
-  ],
-  type: {
-    type: String,
-    required: [true, '"type" is required.'],
-    enum: ['group', 'person'],
+    content: {
+      type: String,
+      required: [true, '"content" is required.'],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    tags: [
+      {
+        type: String,
+        required: [true, '"tag" is required.'],
+      },
+    ],
+    type: {
+      type: String,
+      required: [true, '"type" is required.'],
+      enum: ['group', 'person'],
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: {
+      type: Number,
+      default: 0,
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  comments: {
-    type: Number,
-    default: 0,
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 const Post = mongoose.model('Post', postScheme);
 module.exports = Post;
